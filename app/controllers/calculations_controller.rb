5f36fad448fe623f40e82d7e7a61d53dd64a010a
Jackie DiMonte <jdimonte@chicagobooth.edu>
2017-07-15 03:46:18 +0000

    jk

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -5,8 +5,8 @@ class CalculationsController < ApplicationController
        # The incoming parameter for this action look like {"number" => "5"}
        # Rails stores that hash in a variable called params 
        
-       @input_number = params["number"].to_i
-       @number_squared = params["number"].to_i ** 2
+       @input_number = params["number"].to_f
+       @number_squared = params["number"].to_f ** 2
        # to dive into a hash you use square brackets and the key 
        
        render ("calculations/flexible_square_template.html.erb")
@@ -15,8 +15,8 @@ class CalculationsController < ApplicationController
     
     def flex_square_root
        
-       @input_number = params["number"].to_i
-       @number_square_rooted = params["number"].to_i
+       @input_number = params["number"].to_f
+       @number_square_rooted = params["number"].to_f
        @number_square_rooted = Math.sqrt(@number_square_rooted)
 
        render ("calculations/flexible_square_root_template.html.erb")
@@ -56,8 +56,8 @@ class CalculationsController < ApplicationController
     
     def process_square
        
-       @input_number = params["user_number"].to_i
-       @number_squared = params["user_number"].to_i ** 2
+       @input_number = params["user_number"].to_f
+       @number_squared = params["user_number"].to_f ** 2
 
        render ("calculations/square_results.html.erb")
        
@@ -71,10 +71,30 @@ class CalculationsController < ApplicationController
     
     def process_square_root
        
-       @input_number = params["user_number"].to_i
+       @input_number = params["user_number"].to_f
        @number_square_rooted = Math.sqrt(@input_number)
 
        render ("calculations/square_root_results.html.erb")
+       
+    end
+    
+    def payment_form
+       
+       render ("calculations/payment_form_template.html.erb")
+       
+    end
+    
+    def process_payment
+       
+       @basis_points = params["basis_points"].to_f
+       @r = @basis_points / 12 
+       @years = params["number_of_years"].to_f
+       @pv = params["present_value"].to_f
+       
+       @payment = ( @r * @pv ) / ( 1 - ((1 + @r) ** (-1 * @years)))
+       @payment = @payment.to_f
+
+    render ("calculations/payment_results.html.erb")
        
     end
 
